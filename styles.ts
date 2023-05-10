a99075dadee98165711b8a6e33a761db4cffc779
IogaMaster <iogamastercode@gmail.com>
2023-05-09 22:40:15 -0600

    Add styles.ts

---
+++ styles.ts
@@ -0,0 +1,20 @@
+export const Colors = {
+    text: '#cdd6f4',
+    background: '#11111b',
+    base: '#1e1e2e',
+
+    red: '#f38ba8',
+    green: '#a6e3a1',
+    blue: '#89b4fa',
+};
+
+export const BaseFont = {
+    fontFamily: 'JetBrains Mono',
+    fontWeight: 700,
+    fontSize: 28,
+};
+
+export const WhiteLabel = {
+    ...BaseFont,
+    fill: Colors.text,
+};
