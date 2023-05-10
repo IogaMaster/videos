b2d5c5dbceb7572b1f2cffb5efd06b08d829523c
IogaMaster <iogamastercode@gmail.com>
2023-05-09 22:26:39 -0600

    Add "Intro to Nix" source code

---
+++ intro-to-nix/src/project.ts
@@ -0,0 +1,9 @@
+import {makeProject} from '@motion-canvas/core';
+
+import example from './scenes/example?scene';
+import audio from './audio/audio.mp3'
+
+export default makeProject({
+  scenes: [example],
+  audio: audio
+});
