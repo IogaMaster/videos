bdbe89b7828c7a88e8bfbc2bf5b123b47f6a974b
IogaMaster <iogamastercode@gmail.com>
2023-09-12 13:55:24 -0600

    Add roundtable screen to stream-graphics

---
+++ videos/stream-graphics/src/scenes/roundtable.tsx
@@ -0,0 +1,17 @@
+import { Circle, Img, makeScene2D, Rect, Txt } from '@motion-canvas/2d';
+import { all, createRef, createSignal, delay, easeInOutCubic, waitFor, waitUntil } from '@motion-canvas/core';
+
+import { Colors, Container, setupView, WhiteLabel } from '../../../../styles';
+
+// Assets
+import profile from './assets/profile.png';
+
+export default makeScene2D(function*(view) {
+    setupView(view);
+
+    const outlineRef = createRef<Rect>();
+    view.add(<Txt {...WhiteLabel} fontSize={58} rotation={0} y={-472} x={-600}>Voyagers Roundtable:</Txt>);
+
+    yield* waitUntil("End")
+
+});
