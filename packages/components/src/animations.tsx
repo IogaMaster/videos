2be1aae999826082c3c4c787a829bd5f99cf583c
IogaMaster <iogamastercode@gmail.com>
2023-11-30 16:10:42 -0700

    Add some animation functions

--- packages/components/src/animations.tsx
+++ packages/components/src/animations.tsx
@@ -13,5 +13,12 @@ export function* scrollScreenshot(node: Reference<Node>) {
     yield* node().y(-7000, 25, linear)
 }
 
+export function* wiggle(node: Reference<Node>, amplitude: any) {
+    node().rotation(amplitude * 12, 0.3);
+    node().rotation(amplitude * -12, 0.3);
+    node().rotation(0, 0.3);
+};
 
-
+export function* spin(node: Reference<Node>, amplitude: any) {
+    node().rotation(amplitude * 360, 0.3);
+};
